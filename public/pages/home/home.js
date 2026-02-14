287a230588230ca1993d4172534c497827b199fc
Christopher G <tormentionrex@gmail.com>
2026-02-14 15:53:35 -0600

    nueva estructura

--- public/pages/home/home.js
+++
@@ -1,7 +0,0 @@
-console.log('Home page loaded');
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Lógica específica para la página de inicio
-    const contentDiv = document.getElementById('content');
-    contentDiv.innerHTML = '<p>Este es el contenido dinámico de la página de inicio.</p>';
-});
